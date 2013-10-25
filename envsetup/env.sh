11b874ecbc098fbc4317186a3f1ea90e44f35d79
liuhuahang <liuhuahang@xiaomi.com>
2013-10-26 03:02:27 +0800

    add envsetup
    N/A
    
    Change-Id: I4410b3083d65e5e34924134979eff676acedfba0
    Signed-off-by: liuhuahang <liuhuahang@xiaomi.com>

---
+++ envsetup/env.sh
@@ -0,0 +1,25 @@
+#!/bin/usr/env bash
+
+_find_blade_root ()
+{
+    local dir
+    dir=$PWD;
+    while [ "$dir" != "/" ]; do
+        if [ -f "$dir/BLADE_ROOT" ]; then
+            echo "$dir"
+            return 0
+        fi;
+        dir=`dirname "$dir"`
+    done
+    return 1
+}
+
+export BLADE_ROOT=`_find_blade_root`
+
+if [ $BLADE_ROOT ]; then
+    export PATH=$BLADE_ROOT/tools/typhoon-blade:$PATH
+    export PATH=$BLADE_ROOT/tools/devtools/gyp:$PATH
+    export PATH=$BLADE_ROOT/tools/devtools/header-extractor/bin:$PATH
+    export PATH=$BLADE_ROOT/tools/devtools/wrk:$PATH
+    source $BLADE_ROOT/tools/typhoon-blade/bladefunctions
+fi;
