823f9555a7f22b85843921bcd76e301660be8a25
liuhuahang <liuhuahang@xiaomi.com>
2013-11-13 20:50:25 +0800

    exclude blade from path since typhoon-blade/install conflicts with /usr/bin/install
    N/A
    
    Change-Id: I58e56b6009dd34f1330285570b13ec11cf20276b
    Signed-off-by: liuhuahang <liuhuahang@xiaomi.com>

--- envsetup.sh
+++ envsetup.sh
@@ -18,12 +18,10 @@ BLADE_ROOT=`find_blade_root`
 
 if [ $BLADE_ROOT ]; then
     export BLADE_ROOT
-    export PATH=$BLADE_ROOT/tools/typhoon-blade:$PATH
     export PATH=$BLADE_ROOT/tools/devtools/gyp:$PATH
     export PATH=$BLADE_ROOT/tools/devtools/header-extractor/bin:$PATH
     export PATH=$BLADE_ROOT/tools/devtools/wrk:$PATH
     export PATH=$BLADE_ROOT/tools/devtools/ninja:$PATH
-    source $BLADE_ROOT/tools/typhoon-blade/bladefunctions
 else
     echo "Can't find the file 'BLADE_ROOT' in this or any upper directory."
     echo "Blade need this file as a placeholder to locate the root source directory "
